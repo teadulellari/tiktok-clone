ea5b2758ecf4732c03e7ee138abf931c085a69cd
teadulellari <teadulellari@gmail.com>
2023-01-07 21:34:39 +0200

    added the video footer and sidebar functionality

--- src/Video.js
+++ src/Video.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import "./Video.css";
 import tiktokVideo from "./tiktok_local.mp4";
 function Video() {
+  const [playing, setPlaying] = useState(false);
+  const videoRef= useRef(null);
+
+  const handleVideoPress = () => {
+    //if video is playing
+
+    if(playing) {
+      videoRef.current.pause();
+      setPlaying(false);
+
+    }else{
+      videoRef.current.play();
+      setPlaying(true);
+    }
+    //stop it
+
+
+    //otherwhise if it is not playing
+    //play it...
+  }
   return (
     <div className="Video">
       <video
+      onClick ={handleVideoPress}
       className="video_player"
       loop
+      ref={ videoRef }
       controls
        src={tiktokVideo}></video>
       {/* VideoFooter */}
---
+++ src/VideoFooter.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './VideoFooter.css'
+
+function VideoFooter() {
+    return <div className="videoFooter"></div>
+}
+
+export default VideoFooter;
