114598c2dd3d9c3690ced97d5b1fdc5fc5b6cdd5
teadulellari <teadulellari@gmail.com>
2023-01-09 00:36:09 +0200

    estabilished the backend files and set up the environment

--- tiktok-backend/server.js
+++ tiktok-backend/server.js
@@ -1,26 +1,21 @@
 import express from "express";
 import mongoose from "mongoose";
-import data from './data.js';
+import data from "./data.js";
 
 //app config
 const app = express();
 const port = 9000;
 
-
 //middlewares
 
-
 //DB config
-
-
 
 //api endpoints
 app.get("/", (req, res) => res.status(200).send("Hello World"));
 
-
-app.get('/v1/posts', (req, res) => {
-    res.status(200). send(data)
-})
+app.get("/v1/posts", (req, res) => {
+  res.status(200).send(data);
+});
 
 //listener
 app.listen(port, () => console.log(`listening on localhost : ${port}`));
