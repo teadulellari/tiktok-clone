8f562e416d9c999e9097d620950d6f397fcbce55
teadulellari <teadulellari@gmail.com>
2023-01-09 00:34:00 +0200

    estabilished the backend files and set up the environment

---
+++ tiktok-backend/data.js
@@ -0,0 +1,34 @@
+import tiktokVideo1 from "./tiktok_local.mp4";
+import tiktokVideo2 from "./tiktok_local2.mp4";
+import tiktokVideo3 from "./tiktok_local3.mp4";
+import tiktokVideo4 from "./tiktok_local4.mp4";
+const data = [
+    {
+        url: tiktokVideo1,
+        channel: "oldFoodEater",
+        song: "my song",
+        likes: "",
+        messages: "",
+        description: "",
+        shares: ""
+    },
+    {
+        url: tiktokVideo2,
+        channel: "oldFoodEater",
+        song: "my song",
+        likes: "3456",
+        messages: "65",
+        description: "my post",
+        shares: "34"
+    },
+    {
+        url: tiktokVideo3,
+        channel: "oldFoodEater",
+        song: "my song",
+        likes: "542",
+        messages: "456",
+        description: "my post",
+        shares: "54"
+    },
+]
+export default data;
